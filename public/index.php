97ab79e607acc74314df27e21bdeb97ad32a130e
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2026-02-19 15:40:16 -0300

    Add basic routing system in index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    //SISTEMA DE ROTAS V0.1
+   $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+   switch($uri){
+    case '/teste':
+        print 'teste!';
+        break;
+
+    case '/jogadores':
+        
+        
+        break;
+
+    default:
+    http_response_code(404);
+    print'pagina não encontrada';
+        break;
+
+
+   }
